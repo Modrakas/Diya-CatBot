cc554652b0184f8102ea9c2b3e985b9d9eac047f
modrakas <asmodrak@gmail.com>
2024-12-18 12:06:40 -0500

    added image upload capability/ image styling and fixed 'behavior' typo

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const messageBody = document.querySelector('.message-body');
 const messageInput = document.querySelector('.message-input');
 const sendMessageButton = document.querySelector('#send-message');
+const fileUpload = document.querySelector('#file-upload');
 
 //API setup
 const API_KEY = 'AIzaSyAkD3-q8Jwc9D3KiblnRaT8OAgMXFUCuAI';
@@ -9,7 +10,11 @@ const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-
 
 
 const userData = {
-	message: null
+	message: null,
+	file: {
+		data: null,
+		mime_type: null
+	}
 }
 
 //create div and class
@@ -39,7 +44,7 @@ const generateCatbotResponse = async (incomingMessageDiv) => {
 		headers: {'Content-Type': 'application/json'},
 		body: JSON.stringify({
 			contents: [{
-				parts: [{ text: userData.message }]
+				parts: [{ text: userData.message }, ...(userData.file.data ? [{ inline_data: userData.file }] : [])]
 			}]
 		})
 	}
@@ -68,12 +73,12 @@ const outgoingMessage = (event) => {
 	messageInput.value = '';
 
 	// create and display user message
-	const messageContent = `<div class="message-text"></div>`;
+	const messageContent = `<div class="message-text"></div> ${userData.file.data ? `<img src='data:${userData.file.mime_type};base64,${userData.file.data}' class='attachment'/>` : ''}`;
 
 	const outgoingMessageDiv = createMessageElement(messageContent, 'user-message');
 	outgoingMessageDiv.querySelector('.message-text').textContent = userData.message;
 	messageBody.appendChild(outgoingMessageDiv);
-	messageBody.scrollTo({ top: messageBody.scrollHeight, behavio: 'smooth' }); 
+	messageBody.scrollTo({ top: messageBody.scrollHeight, behavior: 'smooth' }); 
 	
 	//Simulate catbot response with thinking indicator after a delay
 	setTimeout(() => {
@@ -102,4 +107,29 @@ messageInput.addEventListener('keydown', (event) => {
 	}
 });
 
+//Recieve file input
+fileUpload.addEventListener('change', () => {
+	const file = fileUpload.files[0];
+	if(!file) return;
+
+	//Convert file to base64 format so HTTP requests can read the plain text of file
+	const reader = new FileReader();
+	reader.onload = (event) => {
+		const base64String = event.target.result.split(',')[1];
+
+		//Stores file data in userData
+		userData.file = {
+			data: base64String,
+			mime_type: file.type
+		}
+		
+		//Clear file upload value so same file can be uploaded again
+		fileUpload.value = '';
+	}
+	reader.readAsDataURL(file);
+});
+
+//BUTTONS SENDING DATA TO CATBOT
 sendMessageButton.addEventListener('click', (event) => outgoingMessage(event));
+//Trigger file upload when the upload button is clicked
+document.querySelector('#file-upload-btn').addEventListener('click', () => fileUpload.click());
